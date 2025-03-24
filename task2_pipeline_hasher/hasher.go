db29bc02d282cc686f0da36404a9f559a1c67d38
m1keee3 <skirdamisa@gmail.com>
2025-03-24 17:47:58 +0300

    modified: hasher.go

--- task2_pipeline_hasher/hasher.go
+++ task2_pipeline_hasher/hasher.go
@@ -7,15 +7,22 @@ import (
 
 func main() {
 
+	inputData := []int{0, 1}
+
 	hashSignJobs := []job{
 		job(func(in, out chan interface{}) {
-			out <- 1
+			for _, fibNum := range inputData {
+				out <- fibNum
+			}
 		}),
+		job(SingleHash),
+		job(MultiHash),
+		job(CombineResults),
 		job(func(in, out chan interface{}) {
 			dataRaw := <-in
-			data, ok := dataRaw.(int)
+			data, ok := dataRaw.(string)
 			if !ok {
-				fmt.Println("dataRaw is not a string")
+				fmt.Println("cant convert result data to string")
 			}
 			fmt.Println(data)
 		}),
@@ -60,47 +67,76 @@ func ExecutePipeline(jobs ...job) {
 }
 
 func SingleHash(in, out chan interface{}) {
-	dataRaw := <-in
-	data, ok := dataRaw.(string)
-	var crcData, crcData2, mdData string
-	if !ok {
-		fmt.Println("dataRaw is not a string")
-	}
-	go func() {
-		crcData = DataSignerCrc32(data)
-	}()
-	go func() {
-		mdData = DataSignerMd5(data)
-	}()
-	go func() {
-		crcData2 = DataSignerCrc32(mdData)
-	}()
+	mu := &sync.Mutex{}
+	for dataRaw := range in {
+		go func() {
+			data, ok := dataRaw.(string)
+			if !ok {
+				fmt.Println("dataRaw is not a string, SingleHash")
+			}
 
-	out <- crcData + "~" + crcData2
+			crcChan1 := make(chan string)
+			crcChan2 := make(chan string)
+			go func() {
+				crcChan1 <- DataSignerCrc32(data)
+			}()
+			go func() {
+				mu.Lock()
+				mdData := DataSignerMd5(data)
+				mu.Unlock()
+				crcChan2 <- DataSignerCrc32(mdData)
+			}()
+			crc1 := <-crcChan1
+			crc2 := <-crcChan2
+			out <- data
+			out <- crc1 + "~" + crc2
+		}()
+
+	}
 }
 
 func MultiHash(in, out chan interface{}) {
-	dataRaw := <-in
-	data, ok := dataRaw.(string)
-	if !ok {
-		fmt.Println("dataRaw is not a string")
-	}
-	resHashes := make([]string, 6)
-	for i := 0; i < 6; i++ {
+	for dataRaw := range in {
+		singleHashData := <-in
 		go func() {
-			resHashes[i] = DataSignerCrc32(string(rune(i)) + data)
+			wg := &sync.WaitGroup{}
+			wg.Add(6)
+			data, ok := dataRaw.(string)
+			if !ok {
+				fmt.Println("dataRaw is not a string, MultiHash")
+			}
+			resHashs := make([]string, 6)
+			for i := 0; i < 6; i++ {
+				go func() {
+					defer wg.Done()
+					resHashs[i] = DataSignerCrc32(string(rune(i)) + data)
+				}()
+			}
+			wg.Wait()
+
+			var res string
+			for _, hash := range resHashs {
+				res += hash
+			}
+			out <- singleHashData
+			out <- res
 		}()
 	}
-
-	var res string
-
-	for _, hash := range resHashes {
-		res += hash
-	}
-	out <- res
-
 }
 
 func CombineResults(in, out chan interface{}) {
+	for dataRow1 := range in {
+		dataRow2 := <-out
+		singleHashData, ok := dataRow1.(string)
+		if !ok {
+			fmt.Println("dataRow1 is not a string, CombineResults")
+		}
+		multiHashData, ok := dataRow2.(string)
+		if !ok {
+			fmt.Println("dataRow2 is not a string, CombineResults")
+		}
+
+		out <- singleHashData + multiHashData
+	}
 
 }
